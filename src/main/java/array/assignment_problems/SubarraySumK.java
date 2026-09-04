22d05e48c7cfe769ef874668e6a1dda7010bf34c
singhtulsi19 <singhtulsi1919@gmail.com>
2026-09-05 00:51:42 +0530

    Uploading answers for assignment problems

---
+++ src/main/java/array/assignment_problems/SubarraySumK.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class SubarraySumK {
+
+    static int subarraySum(int[] nums, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            sum += num;
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        int result = subarraySum(nums, k);
+
+        System.out.println("Number of subarrays: " + result);
+    }
+}
